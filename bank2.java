1635fef0a3c59ba396c89e844a3e3f2c6ad00fe3
Viniciusrjm <128532862+Viniciusrjm@users.noreply.github.com>
2023-04-18 21:08:06 -0300

    Create bank2.java

---
+++ bank2.java
@@ -0,0 +1,17 @@
+public class banco2 {
+    public static void main(String[] args){
+    banco1 easy = new banco1();
+
+    easy.nome();
+    easy.cpf();
+    easy.numerodaconta();
+    easy.saldo();
+    easy.sacar();
+    easy.depositar();
+
+    easy.exibir();
+
+
+    }
+
+    }
