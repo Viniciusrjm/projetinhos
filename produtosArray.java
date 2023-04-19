19b82ed7c199ab3e02c0aaf6328672244ee614e2
Viniciusrjm <128532862+Viniciusrjm@users.noreply.github.com>
2023-04-18 21:08:59 -0300

    Create produtosArray.java

---
+++ produtosArray.java
@@ -0,0 +1,19 @@
+public class gang {
+ public static void main(String[] args) {
+
+String[] produtos = {"bloco","cimento","tubo","argamassa","gravilhao"};
+int[] quantidade = {100,30,20,10,200};
+double[] preço = {1.25,50,10,26.50,50};
+
+
+for(int i = 0 ; i < produtos.length; i++){
+    System.out.println("Produto: " + produtos[i] + " "+ " Quantidade: " + quantidade [i] + " " + " Preço: R$ " +  preço [i] );
+}
+
+
+
+
+        
+    }
+    
+}
